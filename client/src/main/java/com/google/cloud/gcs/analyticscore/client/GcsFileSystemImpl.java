6bbd9eb04c29118ab18e110b4fa58cc8ca2bf9d9
Shubham Diwakar <shubhamdiwakar@google.com>
2025-08-26 05:55:38 -0700

    Implement GCS File System and add other required utilities.
    Change-Id: I14bf1501467bf7fef93bc7bf357886efc4f567bd

---
+++ client/src/main/java/com/google/cloud/gcs/analyticscore/client/GcsFileSystemImpl.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.gcs.analyticscore.client;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import java.net.URI;
+import com.google.auth.Credentials;
+import java.io.IOException;
+import java.util.Collections;
+import com.google.common.annotations.VisibleForTesting;
+
+public class GcsFileSystemImpl implements GcsFileSystem {
+
+    private final GcsClient gcsClient;
+    private final GcsFileSystemOptions fileSystemOptions;
+
+    public GcsFileSystemImpl(Credentials credentials, GcsFileSystemOptions fileSystemOptions) {
+        this.gcsClient = new GcsClientImpl(credentials, fileSystemOptions.getGcsClientOptions());
+        this.fileSystemOptions = fileSystemOptions;
+    }
+
+    @VisibleForTesting
+    GcsFileSystemImpl(GcsClient gcsClient, GcsFileSystemOptions fileSystemOptions) {
+        this.gcsClient = gcsClient;
+        this.fileSystemOptions = fileSystemOptions;
+    }
+
+    @Override
+    public VectoredSeekableByteChannel open(URI path, GcsReadOptions readOptions) throws IOException {
+        checkNotNull(path, "path should not be null");
+        GcsItemId itemId = UriUtil.getItemIdFromString(path.toString());
+        checkArgument(itemId.isGcsObject(), "Expected GCS object to be provided. But got: " + itemId);
+        return gcsClient.openReadChannel(itemId, readOptions);
+    }
+
+    @Override
+    public GcsFileInfo getFileInfo(URI path) throws IOException {
+        checkNotNull(path, "path should not be null");
+        GcsItemId itemId = UriUtil.getItemIdFromString(path.toString());
+        GcsItemInfo gcsItemInfo = gcsClient.getGcsItemInfo(itemId);
+        return GcsFileInfo.builder()
+                .setItemInfo(gcsItemInfo)
+                .setUri(path)
+                .setAttributes(Collections.emptyMap())
+                .build();
+    }
+
+    @Override
+    public GcsFileSystemOptions getFileSystemOptions() {
+        return this.fileSystemOptions;
+    }
+
+    @Override
+    public GcsClient getGcsClient() {
+        return this.gcsClient;
+    }
+}
