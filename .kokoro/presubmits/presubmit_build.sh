69158dd4aa9ee057211b18708ff792ab6f8dfff6
Ajay Yadav <ajayky@google.com>
2025-09-12 15:24:03 +0530

    remove kokoro configurations from repository (#82)

--- .kokoro/presubmits/presubmit_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Fail on any error.
-set -e
-
-cd "${KOKORO_ARTIFACTS_DIR}/github/gcs-analytics-core"
-./mvnw -Pintegration-test clean verify -Dmaven.javadoc.skip=true -Dsource.skip=true  -Dgpg.skip=true
